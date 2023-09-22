3b23555d49b3c87fcae4e6b6b9f6c5fd4512e181
DaveyDark <devesh97531@gmail.com>
2023-09-22 13:45:48 +0530

    added db link

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const sqlite3 = require('sqlite3')
 
 const db = new sqlite3.Database('onerodha.db')
@@ -6,9 +7,17 @@ const db = new sqlite3.Database('onerodha.db')
 app = express()
 
 app.use(express.static('static'))
-
+app.use(session({
+    secret: 'finance',
+    resave: false,
+    saveUninitialized: true
+}));
 app.set('view engine', 'pug')
+
 app.get('/', (req,res) => {
+  if(!req.session.username) {
+    return res.redirect('/login');
+  }
   res.render('index')
 })
 app.get('/login', (req,res) => {
