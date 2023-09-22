a4059246a1f1b7a7ea31f2a9d4569f45bfe79ef0
DaveyDark <devesh97531@gmail.com>
2023-09-22 12:15:19 +0530

    Added login route

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const sqlite3 = require('sqlite3')
+
+const db = new sqlite3.Database('onerodha.db')
 
 app = express()
 
@@ -7,6 +10,9 @@ app.use(express.static('static'))
 app.set('view engine', 'pug')
 app.get('/', (req,res) => {
   res.render('index')
+})
+app.get('/login', (req,res) => {
+  res.render('login')
 })
 
 app.listen(5000)
